e7a3cb8f85d1d03b3fd3e81051d15d46cd041599
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-08-19 11:44:49 +0400

    implement for loop to array

--- javaScript/javascript2/warmup.js
+++ javaScript/javascript2/warmup.js
@@ -18,3 +18,17 @@ console.log(city);
  console.log(city.length);
 
 
+ //for loop, then again with for...of 
+
+for( i=0; i<=city.length; i++)
+{
+    console.log(city[i]);
+}
+
+
+for( const cities of city)
+{
+    console.log(city)
+ 
+}
+

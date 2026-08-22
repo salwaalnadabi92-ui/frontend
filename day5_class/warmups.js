d000ec0cb112c8aafbf6a119b2442bba23946102
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-08-22 18:22:49 +0400

    implement deferent || and ??

--- day5_class/warmups.js
+++ day5_class/warmups.js
@@ -6,4 +6,11 @@
 //Nullish Coalescing ??
 const theme = settings.theme ?? "light";
 
-console.log(theme);
+console.log(theme);//output light
+
+
+const discount = 0;
+
+console.log(discount || 10);//output :10
+
+console.log(discount ?? 10);//output :0

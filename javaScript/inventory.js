b2966344a7bf831e8f6692b68210f1367bbc8358
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-08-22 21:33:17 +0400

    add array of object

---
+++ javaScript/inventory.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        name: "T-Shirt",
+        price: 15,
+        stock: 5,
+        category: "Clothing"
+    },
+    {
+        name: "Jeans",
+        price: 30,
+        stock: 15,
+        category: "Clothing"
+    },
+    {
+        name: "Jacket",
+        price: 50,
+        stock: 8,
+        category: "Clothing"
+    },
+    {
+        name: "Sofa",
+        price: 300,
+        stock: 12,
+        category: "Furniture"
+    },
+    {
+        name: "Dining Table",
+        price: 200,
+        stock: 4,
+        category: "Furniture"
+    },
+    {
+        name: "Curtains",
+        price: 40,
+        stock: 20,
+        category: "Home"
+    }
+];

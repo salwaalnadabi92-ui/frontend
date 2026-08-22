0c0d3f14b8c0cd6d7ba24c9c2b3edcafd32289a7
salwaalnadabi92-ui <salwaalnadabi92@gmail.com>
2026-08-22 18:40:14 +0400

    implement extend and super

--- day5_class/warmups.js
+++ day5_class/warmups.js
@@ -40,3 +40,22 @@ const employee1 = new Employee("Sara", "Developer");
 
 console.log(employee1.getGreeting());
 
+//
+
+class Manager extends Employee {
+    constructor(name, role, teamSize) {
+        super(name, role);
+        this.teamSize = teamSize;
+    }
+
+
+getgreeting()
+{
+
+         return ` my name is ${this.name} and I am a ${this.role} i am  mange  ${this.teamSize} of pepole  `;
+}
+
+}
+
+const employee2=new employee("Sara", "Developer", 8 );
+console.log(getgreeting());
